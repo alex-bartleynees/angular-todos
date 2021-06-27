feddc46ee9b3d0c40d590cafd9ba1107807636a7
Alex Bartley Nees <alexbartleynees@gmail.com>
2021-06-28 09:58:51 +1200

    add mock task data"

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'June 28th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Dinner',
+        day: 'July 1st at 6:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Clean Room',
+        day: 'June 28th at 4:30pm',
+        reminder: false,
+    }
+]
